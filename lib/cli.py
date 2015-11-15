2bc49d48298d02506257192906303cb5d2a033af
fisk <mcfisk@ymail.com>
2015-11-15 00:20:53 -0600

    added cli arguments in lib/cli.py, also added a temporary list of things to backup once the program is mature enough to handle this

--- lib/cli.py
+++ lib/cli.py
@@ -1,11 +1,21 @@
 import argparse
 
-# parse system arguments to decide best course of action to take
 parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers()
 
-### ARGUMENTS ###
-# Add or remove commandline arguments here
-parser.add_argument("-m", "--manifest", help="Specify a diffrent manifest file fo read from", action="store")
+#parser.add_argument("status", action="store_true")
 
-### END ARGUMENTS ###
+status = subparser.add_parser("status")
+
+#manifest = parser.add_group()
+manifest = subparser.add_parser("manifest")
+#manifest.add_argument("--Manifest", "-M", action="store_true", help="Set Tarpacker to manifest mode to add or remove files from the manifest, aswell as change the manifest file location.")
+manifest.add_argument("--add", "-a", action="store", help="In manifest mode, add a file to the manifest. Format is {file:group}. Group must be preexisting, use -f to auto-add an unknown group or -g to add the group before hand.")
+manifest.add_argument("--force", "-f", action="store_true", help="In manifest mode, forcefuly add a file to an unknown group by first creating the unknown group and then adding the file to it")
+manifest.add_argument("--add-group", "-g", action="store", help="In manifest mode, add a group to the manifest")
+manifest.add_argument("--set", "-s", action="store", help="In manifest mode, set the manifest file location, the file is created if it does not exist.\n All manifest files are initiated with the group \"Misc\" all ready added.")
+
+config = subparser.add_parser("config")
+config.add_argument("--place-holder", action="store_true", help="does nothing")
+
 args = parser.parse_args()
