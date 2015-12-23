2b3ff87bfa586ed6a636765409e320a76d523271
fisk <mcfisk@ymail.com>
2015-12-23 17:57:57 -0600

    Created modules for every mode. All mode specific functionality will be made in its own class

--- tp.py
+++ tp.py
@@ -1,29 +1,27 @@
 #! /usr/bin/python3
 
-import os, tarfile, logging
-from lib.cli import *
-from lib.mplib import *
+import os
+from lib.cli   import *
+
+from lib.mode.status   import Status
+from lib.mode.manifest import Manifest
+from lib.mode.pack     import Pack
+from lib.mode.unpack   import Unpack
+from lib.mode.config   import Config
 
 class TarPacker():
     def __init__(self):
-        self.preProcess()
-
-    def modeManifest(self):
-        if args.status:
-            self.mparser = ManifestParser("manifest.mf")
-            self.mparser.parseManifest()
-            print(self.mparser)
-
-    def preProcess(self):
         #determine the mode and run proper function
-        if args.subparser_name == "manifest":
-            self.modeManifest()
-
-    def createBackup(self):
-        pass
-
-    def restoreBackup(self):
-        pass
+        if args.subparser_name == "status":
+            Status(args)
+        elif args.subparser_name == "manifest":
+            Manifest(args)
+        elif args.subparser_name == "pack":
+            Pack(args)
+        elif args.subparser_name == "unpack":
+            Unpack(args)
+        elif args.subparser_name == "config":
+            Config(args)
 
 if __name__ == "__main__":
     main = TarPacker()
