2707e0382e5c0dcf25fd6bf53c4fb762f20f0a8a
fisk <mcfisk@ymail.com>
2016-08-19 17:08:02 -0500

    beginning programming of real mainfest mode functionality, --status works

--- lib/mode/manifest.py
+++ lib/mode/manifest.py
@@ -7,7 +7,49 @@ TODO:
 """
 
 import os
+from lib.config import Config
+from lib.mplib  import ManifestParser
 
 class Manifest():
     def __init__(self, args, setup):
-        os.system("vim {0}".format(setup.manifestdir))
+        # Create config object
+        self.conf  = Config(setup.configdir+"/settings.json")
+
+        # flags...
+        self.args  = args
+
+        # Create ManifestParser
+        self.manifest = self.argsInFile()
+        self.manifest.parseManifest()
+
+        # Directories
+        self.setup = setup
+
+        print(self.args)
+
+        #self.manifest.data["misc"].append("/home/fisk/.config/tarpacker/settings.json")
+        #self.manifest.generateManifest()
+        
+        self.argsEdit()
+        self.argsStatus()
+        self.addManifestEntry()
+
+    def addManifestEntry(self):
+        pass
+
+    def argsStatus(self):
+        if self.args.status:
+            self.manifest.info()
+
+    def argsInFile(self):
+        if self.args.in_file:
+            return ManifestParser(self.args.in_file)
+        else:
+            return ManifestParser(self.conf.data["manifest"]["file"])
+
+    def argsEdit(self):
+        if self.args.edit:
+            self.manualEdit()
+
+    def manualEdit(self):
+        os.system("vim {0}".format(self.setup.manifestdir))
